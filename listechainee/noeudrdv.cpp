52b0193e8eb39a0bc08e00c9d63d9ee0cbbcfbdc
acazals <165204171+acazals@users.noreply.github.com>
2024-12-13 13:29:23 +0100

    Update noeudrdv.cpp

--- listechainee/noeudrdv.cpp
+++ listechainee/noeudrdv.cpp
@@ -1,6 +1,6 @@
 #include "RDV.h"
-#include "date.h"  // Inclure le fichier d'en-tête Date
-#include "heure.h" // Inclure le fichier d'en-tête Heure
+#include "date.h"  //
+#include "heure.h" //  fichier d'en-tête Heure
 // je recupere les fonctions de date et heure et RDV
 #include <iostream>
 #include <string> 
